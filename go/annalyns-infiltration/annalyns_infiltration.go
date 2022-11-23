fcb6e615fe577630cbc055d0baf3a3421355e3bd
Supermike <superdafe@gmail.com>
2022-11-23 12:48:00 +0100

    annalyns-infiltration first iteration

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -2,21 +2,43 @@ package annalyn
 
 // CanFastAttack can be executed only when the knight is sleeping.
 func CanFastAttack(knightIsAwake bool) bool {
-	panic("Please implement the CanFastAttack() function")
+    canAttack := true
+    if knightIsAwake {
+        canAttack = false
+    }
+    return canAttack;
 }
 
 // CanSpy can be executed if at least one of the characters is awake.
 func CanSpy(knightIsAwake, archerIsAwake, prisonerIsAwake bool) bool {
-	panic("Please implement the CanSpy() function")
+    canSpy := false
+    if knightIsAwake {
+        canSpy = true
+    } else if archerIsAwake {
+        canSpy = true
+    } else if prisonerIsAwake {
+        canSpy = true
+    }
+    return canSpy;
 }
 
 // CanSignalPrisoner can be executed if the prisoner is awake and the archer is sleeping.
 func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
-	panic("Please implement the CanSignalPrisoner() function")
+    canSignal := false
+    if prisonerIsAwake && !archerIsAwake {
+        canSignal = true
+    }
+    return canSignal
 }
 
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	panic("Please implement the CanFreePrisoner() function")
+    canFree := false
+    if prisonerIsAwake && !archerIsAwake && !knightIsAwake {
+        canFree = true
+    } else if petDogIsPresent && !archerIsAwake { 
+        canFree = true
+    }
+    return canFree
 }
